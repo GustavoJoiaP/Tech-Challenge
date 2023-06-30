d537ed5486989b155a3f10eec60cc8d2adf2a4cb
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2023-06-30 09:44:35 -0300

    Primeiro commit, foi criado as pastas e o projeto de dominio

---
+++ src/Dominio/Programa.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechChallenge
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

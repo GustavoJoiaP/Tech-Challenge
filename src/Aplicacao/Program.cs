50489d4bc3b6784b3a77590fd5f36709d89568f8
Gustavo Joia Paziani <joia.gustavo@gmail.com>
2023-06-30 14:59:05 -0300

    Criado o projeto de aplicacao e adicionado na solução

---
+++ src/Aplicacao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
